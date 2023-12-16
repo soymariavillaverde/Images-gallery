e1a01e8a6d384b053ecc87e079cf59a5eb1875c1
soymariavillaverde <soymariavillaverde@gmail.com>
2023-12-16 23:42:09 +0100

    Created CRUD methods in java, may have some issues with delete method :construction:

--- src/main/java/com/factoriaf5/imggallery/controllers/CardController.java
+++ src/main/java/com/factoriaf5/imggallery/controllers/CardController.java
@@ -5,12 +5,17 @@ import com.factoriaf5.imggallery.models.CardModel;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -35,4 +40,25 @@ public class CardController {
         return this.cardService.saveCard(card);
     }
     
+    @GetMapping(path = "/{id}")
+    public  Optional<CardModel>getImageById(@PathVariable("id") Long id) {
+        return this.cardService.getCardById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public CardModel updateCardById(@RequestBody CardModel request, @PathVariable("id") Long id){
+        return this.cardService.updateCardById(request, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String CardModel deleteCardById(@PathVariable("id") Long id){
+        boolean ok = this.cardService.deleteImg(id);
+
+        if(ok){
+            return "Image with id " + id + " was succesfully deleted";
+        } else {
+            return "Oooops, something went wrong and your image with id " + id + " was not deleted";
+        }
+    }
 }
+`
--- src/main/java/com/factoriaf5/imggallery/services/CardService.java
+++ src/main/java/com/factoriaf5/imggallery/services/CardService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.factoriaf5.imggallery.models.CardModel;
 import com.factoriaf5.imggallery.repositories.ICardRepository;
+import com.google.common.base.Optional;
 
 @Service
 public class CardService {
@@ -23,9 +24,30 @@ public class CardService {
             return cardRepository.save(card);
     }
 
-    public java.util.Optional<CardModel> getImageById(Long id){
+    // obtener una sola img
+    public java.util.Optional<CardModel> getCardById(Long id){
         // optional permite que este método pueda devolver algo, o bien ser null
         return cardRepository.findById(id);
     }
 
+
+    // método put
+    public CardModel updateCardById(CardModel request, Long id){
+        CardModel card = cardRepository.findById(id).get();
+
+        card.setName(request.getName());
+        card.setImg(request.getImg());
+        return card;
+    }
+
+    // método delete
+    public Boolean deleteImg (Long id){
+        try{
+            cardRepository.deleteById(id);
+            return true;
+
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
