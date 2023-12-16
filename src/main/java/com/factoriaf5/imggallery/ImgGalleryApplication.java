81dbabd2c3f973325041cabbfa4ada11a6880d0d
soymariavillaverde <soymariavillaverde@gmail.com>
2023-12-16 13:54:26 +0100

    Starting project and setting up java with springboot, and some dependencies I will need :tada: :construction:

---
+++ src/main/java/com/factoriaf5/imggallery/ImgGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.imggallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/imggallery/ImgGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.imggallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
