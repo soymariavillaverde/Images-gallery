3b17198d1642f414efcf51af5a3d6e7ba455ea23
soymariavillaverde <soymariavillaverde@gmail.com>
2023-12-17 15:20:02 +0100

    installed framework and some others tools to develop my app, and created basic structure :building_construction: :construction:

---
+++ src/main/frontend/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <h1>This is the footer</h1>
+</template>
+
+<style>
+</style>
---
+++ src/main/frontend/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>This is an about page</h1>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+</style>
