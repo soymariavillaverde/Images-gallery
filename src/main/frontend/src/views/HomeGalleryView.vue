ce770cef184c43a6aa31338752d840cc57416b15
soymariavillaverde <soymariavillaverde@gmail.com>
2023-12-18 11:15:21 +0100

    fix some files structure and clean files I won't use :building_construction:

--- src/main/frontend/src/views/HomeGalleryView.vue
+++ src/main/frontend/src/views/HomeGalleryView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import Header from "../components/Header.vue";
+
 </script>
 
 <template>
   <main>
+    <Header></Header>
       <h1>This is the homepage</h1>
+      
   </main>
 </template>
 
